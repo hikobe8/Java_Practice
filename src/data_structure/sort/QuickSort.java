db8ee304fd2e194a9a1a11d358fe9b90f11d850e
hikobe8@163.com <yurui@palmax.cn>
2018-08-10 15:40:23 +0800

    quick sort by iteration

--- src/data_structure/sort/QuickSort.java
+++ src/data_structure/sort/QuickSort.java
@@ -1,13 +1,21 @@
 package data_structure.sort;
 
+import java.util.Stack;
+
 /**
  * Author : Ray
  * Created At : 2018-08-09 下午11:27
  * Email : ryu18356@gmail.com
- * Description : 快速排序（递归实现）
+ * Description : 快速排序
  */
 public class QuickSort {
 
+    /**
+     * 递归实现
+     * @param arr
+     * @param left
+     * @param right
+     */
     private static void quickSort(int[] arr, int left, int right) {
         if (left > right)
             return;
@@ -26,20 +34,50 @@ public class QuickSort {
                 int t = arr[i];
                 arr[i] = arr[j];
                 arr[j] = t;
-                System.out.println();
-                for (int anArr : arr) {
-                    System.out.print(anArr + " ");
-                }
             }
         }
         arr[left] = arr[i];
         arr[i] = standard;
-        System.out.println();
-        for (int anArr : arr) {
-            System.out.print(anArr + " ");
-        }
         quickSort(arr, left, i - 1);
         quickSort(arr, i + 1, right);
+    }
+
+    /**
+     * 迭代实现
+     * 借助两个栈来存取起始坐标完成迭代
+     * @param arr
+     */
+    private static void quickSort(int[] arr) {
+        Stack<Integer> startIndexStack = new Stack<>();
+        Stack<Integer> endIndexStack = new Stack<>();
+        int left = 0;
+        int right = arr.length - 1;
+        startIndexStack.push(left);
+        endIndexStack.push(right);
+        while (!startIndexStack.isEmpty() && !endIndexStack.isEmpty()) {
+            int start = startIndexStack.pop();
+            int end = endIndexStack.pop();
+            if (start < end) {
+                int i = start;
+                int j = end;
+                int standard = arr[start];
+                while (arr[j] <= standard && i < j) {
+                    j --;
+                }
+                while (arr[i] >= standard && i < j) {
+                    i ++;
+                }
+                if (i < j) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                    startIndexStack.push(left);
+                    endIndexStack.push(i - 1);
+                    startIndexStack.push(i + 1);
+                    endIndexStack.push( right);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -50,7 +88,13 @@ public class QuickSort {
         }
         quickSort(arr, 0, arr.length - 1);
         System.out.println();
-        System.out.print("sorted array : ");
+        System.out.print("recursion sorted array : ");
+        for (int anArr : arr) {
+            System.out.print(anArr + " ");
+        }
+        quickSort(arr);
+        System.out.println();
+        System.out.print("iteration sorted array : ");
         for (int anArr : arr) {
             System.out.print(anArr + " ");
         }
