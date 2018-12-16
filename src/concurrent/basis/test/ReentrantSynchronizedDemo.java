b9049112739bd298f6781a64d002ce3bc8ac65aa
hikobe8 <hikobe8@163.com>
2018-12-16 12:49:14 +0800

    add comment

--- src/concurrent/basis/test/ReentrantSynchronizedDemo.java
+++ src/concurrent/basis/test/ReentrantSynchronizedDemo.java
@@ -16,6 +16,7 @@ public class ReentrantSynchronizedDemo {
     private static class LoadWidget extends Widget{
         @Override
         public synchronized void doSomething() {
+            //synchronized关键字为可重入锁，如果设计为不可重入锁的话调用super.doSomething会发生死锁
             System.out.println("doSomething in LoadWidget");
             super.doSomething();
         }
