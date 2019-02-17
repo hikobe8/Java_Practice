140bbf0b423cfb08aa83eb77ec05d3113a5a14ac
hikobe8 <hikobe8@163.com>
2019-02-18 00:16:08 +0800

    add kotlin support & binary search tree

---
+++ src/data_structure/binary_tree/Classes.kt
@@ -0,0 +1,85 @@
+package data_structure.binary_tree
+
+
+/**
+ * Author : Ray
+ * Created At : 2019-02-17 5:25 PM
+ * Email : ryu18356@gmail.com
+ * Description : 二叉树的节点
+ */
+data class Node(var iData: Int, var fData: Double, var leftChild: Node?, var rightChild: Node?) {
+
+    constructor() : this(0, 0.0, null, null)
+
+    fun displayNode() {
+
+    }
+}
+
+class Tree {
+    var root: Node? = null
+
+    fun find(key: Int): Node? {
+        var current: Node? = root
+        while (current?.iData != key) {
+            if (key < current?.iData!!) {
+                current = current.leftChild
+            } else {
+                current = current.rightChild
+            }
+            if (current == null) {
+                return null
+            }
+        }
+        return current
+
+    }
+
+    fun insert(id: Int, dd: Double) {
+        val newNode = Node()
+        newNode.iData = id
+        newNode.fData = dd
+        if (root == null) {
+            //创建根结点
+            root = newNode
+        } else {
+            //开始查找
+            var current = root
+            var parent: Node? = null
+            while (true) {
+                parent = current
+                if (id < current!!.iData) {
+                    current = current.leftChild //移动到左子节点
+                    if (current == null) {
+                        parent!!.leftChild = newNode
+                        return
+                    }
+                } else {
+                    current = current.rightChild
+                    if (current == null) {
+                        parent!!.rightChild = newNode
+                        return
+                    }
+                }
+            }
+        }
+
+    }
+
+    fun delete(id: Int) {
+
+    }
+
+    /**
+     * 中序遍历
+     */
+    fun inOrder(root:Node?) {
+        if (root != null) {
+            inOrder(root.leftChild)
+            print("${root.fData} ")
+            inOrder(root.rightChild)
+        }
+    }
+
+}
+   
---
+++ src/data_structure/binary_tree/TreeApp.kt
@@ -0,0 +1,19 @@
+package data_structure.binary_tree
+
+import com.sun.source.tree.Tree
+
+/**
+ * Author : Ray
+ * Created At : 2019-02-17 5:31 PM
+ * Email : ryu18356@gmail.com
+ * Description :
+ */
+fun main(args:Array<String>) {
+    val tree = Tree()
+    tree.insert(20, 1.2)
+    tree.insert(70, 2.5)
+    tree.insert(30, 3.0)
+    tree.insert(15, 42.0)
+    tree.insert(28, 51.0)
+    tree.inOrder(tree.root)
+}
