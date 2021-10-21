d13eb4ab4fc332da1e8485d2ab2125d1a3d1d3e5
hikobe8@163.com <hikobe8@163.com>
2021-10-22 00:39:56 +0800

    sortparent

---
+++ src/algorithm/redbook/SortParent.java
@@ -0,0 +1,35 @@
+package algorithm.redbook;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * 排序模板
+ */
+public class SortParent {
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = a[i];
+    }
+
+    public static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1]))
+                return false;
+        }
+        return true;
+    }
+
+}
